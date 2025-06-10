4bbb11af140ae883d9b7cdf50fbc321bcf9b95df
yolovi <yolandalopevi@gmail.com>
2025-06-10 20:36:57 +0200

    port dotenv en index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
-const PORT = 8080
+require("dotenv").config()
+const PORT = process.env.PORT
 const { dbConnection } = require("./config/config")
 
 dbConnection()
